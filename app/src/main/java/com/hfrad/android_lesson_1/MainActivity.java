783b491b50279b64c87e37850fadc6f9aac0d881
Viktrosen <bolotov.vit@yandex.ru>
2020-06-25 19:54:22 +0500

    Initial commit

---
+++ app/src/main/java/com/hfrad/android_lesson_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hfrad.android_lesson_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
