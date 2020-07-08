69e7f1282a1f330971a995e1764b296afbadac11
Viktrosen <bolotov.vit@yandex.ru>
2020-07-08 23:48:34 +0500

    Попытался сделать первое задание. Остальные по времени, к сожалению, не успеваю

---
+++ app/src/main/java/com/hfrad/android_lesson_1/Temperature.java
@@ -0,0 +1,43 @@
+package com.hfrad.android_lesson_1;
+
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Temperature extends Fragment {
+
+
+    public Temperature() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_temperature, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
